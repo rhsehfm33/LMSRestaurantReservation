9953cfb16378dd5608647b346ee472046e779587
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-21 16:47:59 +0900

    feat : Implement Restaurant & RestaurantTests classes

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,23 @@
+package kr.co.fastcampus.eatgo.domain;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+class RestaurantTests {
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+    }
+
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+
+        assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+    }
+}
