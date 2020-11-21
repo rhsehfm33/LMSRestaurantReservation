922a6f97e9480ae06a3d78fd2c80978451af0f47
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-21 18:23:33 +0900

    feat : Implement MenuItem entity & repository & impl, Apply test MenuItem to Restaurant test

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -1,5 +1,8 @@
 package kr.co.fastcampus.eatgo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
 
     private final Long id;
@@ -7,6 +10,7 @@ public class Restaurant {
     private final String name;
 
     private final String address;
+    private List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     public Restaurant(Long id, String name, String address) {
         this.id = id;
@@ -28,5 +32,19 @@ public class Restaurant {
 
     public String getAddress() {
         return this.address;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for (MenuItem menuItem : menuItems) {
+            addMenuItem(menuItem);
+        }
     }
 }
