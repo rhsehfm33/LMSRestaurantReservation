a215a303ce5fec2418d5dbc2cbaa69ca392ed075
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-21 17:20:47 +0900

    feat : Implement list method in RestaurantController & test method

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -2,16 +2,24 @@ package kr.co.fastcampus.eatgo.domain;
 
 public class Restaurant {
 
+    private final Long id;
+
     private final String name;
+
     private final String address;
 
-    public Restaurant(String name, String address) {
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
         this.name = name;
         this.address = address;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getInformation() {
