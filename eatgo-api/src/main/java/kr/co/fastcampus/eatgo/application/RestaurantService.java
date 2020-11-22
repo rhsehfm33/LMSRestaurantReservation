face9052d02f8c77da9aa095bd6f9d3dd258872f
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-11-22 22:09:12 +0900

    feat : Implement create method in RestaurantController

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/application/RestaurantService.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/application/RestaurantService.java
@@ -38,4 +38,8 @@ public class RestaurantService {
 
         return restaurant;
     }
+
+    public void addRestaurant(Restaurant restaurant) {
+        //
+    }
 }
--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -5,12 +5,17 @@ import java.util.List;
 
 public class Restaurant {
 
-    private final Long id;
+    private Long id;
 
-    private final String name;
+    private String name;
 
-    private final String address;
+    private String address;
+
     private List<MenuItem> menuItems = new ArrayList<MenuItem>();
+
+    public Restaurant() {
+
+    }
 
     public Restaurant(Long id, String name, String address) {
         this.id = id;
