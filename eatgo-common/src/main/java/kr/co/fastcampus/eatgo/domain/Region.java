4d5cf263d009a29a52368fbe29c8eb864f1799bf
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-12-02 01:08:06 +0900

    feat : Implement get method in RegionController & RegionService & RegionRepository

---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Region.java
@@ -0,0 +1,21 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
