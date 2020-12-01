6609bd5348d4fabe5b6c76e158828320e227968e
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-12-02 00:46:21 +0900

    feat : Add additional annotaions to MenuItemService & MenuItemController

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
@@ -4,11 +4,13 @@ import kr.co.fastcampus.eatgo.domain.MenuItem;
 import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class MenuItemService {
 
     private MenuItemRepository menuItemRepository;
