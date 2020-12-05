c3a3be13a8cba890295caf734d18945872a7e4df
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-12-06 00:46:22 +0900

    feat : Implement function that can watch reservations from restaurant

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/ReservationRepository.java
@@ -3,8 +3,12 @@ package kr.co.fastcampus.eatgo.domain;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+
+    List<Reservation> findAllByRestaurantId(Long restaurantId);
 
     Reservation save(Reservation reservation);
 }
